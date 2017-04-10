c45ac39539c5c16094ade9b0325b551c6211f238
John De Mott <heydemo13@gmail.com>
2017-04-10 15:16:23 -0500

    Make SoundHotspot use aframe-react Entity, pass all props to Entity

--- src/components/SoundHotspot.jsx
+++ src/components/SoundHotspot.jsx
@@ -3,29 +3,37 @@
  * Exports a SoundHotspot component.
  */
 
-import React from 'react';
+import React, { PropTypes } from 'react';
+import { Entity } from 'aframe-react';
 
 /**
  * SoundHotspot that, when clicked, plays a sound.
  */
 const SoundHotspot = props =>
-  (<a-entity
+  (<Entity
+    {...props}
     id={props.id}
-    geometry="primitive: box"
-    material={`src: ${props.image}`}
+    geometry={{ primitive: 'box' }}
+    material={{ src: props.image }}
     sound={`src: url(${props.mp3}); on: click`}
-    position="-10 0 0"
+    position={{ x: -10, y: 0, z: 0 }}
   />);
 
 SoundHotspot.propTypes = {
   id: React.PropTypes.string,
   mp3: React.PropTypes.string.isRequired,
   image: React.PropTypes.string,
+  position: PropTypes.shape({
+    x: React.PropTypes.integer,
+    y: React.PropTypes.integer,
+    z: React.PropTypes.integer,
+  }),
 };
 
 SoundHotspot.defaultProps = {
   id: 'sound-hotspot',
   image: require('../assets/images/play-sound.png'),
+  position: { x: -10, y: 0, z: 0 },
 };
 
 export default SoundHotspot;
