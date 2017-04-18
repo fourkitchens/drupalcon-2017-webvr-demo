9c02b041cbb97206dfe681b4044f27e285fcc682
Patrick Coffey <patrickcoffey48@gmail.com>
2017-04-18 18:53:26 -0500

    Add 404 page image and sound

--- src/components/scenes/NoMatch.jsx
+++ src/components/scenes/NoMatch.jsx
@@ -17,17 +17,22 @@ const NoMatch = {
   sky: require('../../assets/images/scenes/no-match.png'),
   scene: () => (
     <Entity>
-      <Entity>
-        <Modal
-          visible
-          id="modal__404"
-          title="404: Not Found In This Reality"
-          to="#"
-          content="The possibilities for WebVR are infinite, but this scene
-          hasn't been built yet. Go back to where you were and keep exploring!"
-          image={require('../../assets/images/jpg/404-cuteness.jpg')}
-        />
-      </Entity>
+      <Entity
+        primitive="a-sound"
+        src={`src: url(${require('../../assets/sounds/404-ambient.mp3')})`}
+        position={{ x: 1, y: 1, z: -5 }}
+        autoplay
+      />
+      <Modal
+        visible
+        id="modal__404"
+        title="404: Not Found In This Reality"
+        to="#"
+        position={{ x: 1, y: 1, z: -5 }}
+        content="The possibilities for WebVR are infinite, but this scene
+        hasn't been built yet. Go back to where you were and keep exploring!"
+        image={require('../../assets/images/png/404-lego.png')}
+      />
     </Entity>
   ),
 };
