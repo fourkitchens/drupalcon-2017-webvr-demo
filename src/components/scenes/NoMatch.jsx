d8827442fe786681bf836c218d5e6ce31b5b2b6a
Patrick Coffey <patrickcoffey48@gmail.com>
2017-04-18 18:48:45 -0500

    Scott doesnt get his snow, WebGL hates scott :(

--- src/components/scenes/NoMatch.jsx
+++ src/components/scenes/NoMatch.jsx
@@ -8,8 +8,6 @@ import React from 'react';
 
 import Modal from '../Modal.jsx';
 
-require('aframe-particle-system-component');
-
 /**
  * Returns React component that render's a 404 Not Found scene.
  * {@inheritdoc}
@@ -19,16 +17,11 @@ const NoMatch = {
   sky: require('../../assets/images/scenes/no-match.png'),
   scene: () => (
     <Entity>
-      <Entity
-        particle-system="preset: snow; particleCound: 5000;"
-        position={{ x: 0, y: 2.25, z: -15 }}
-      />
       <Entity>
         <Modal
           visible
           id="modal__404"
           title="404: Not Found In This Reality"
-          actionText="Back"
           to="#"
           content="The possibilities for WebVR are infinite, but this scene
           hasn't been built yet. Go back to where you were and keep exploring!"
