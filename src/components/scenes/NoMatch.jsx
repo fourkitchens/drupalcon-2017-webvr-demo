ad01663fc411fd9927c7d2b3ba4e1307a67056d2
Patrick Coffey <patrickcoffey48@gmail.com>
2017-04-18 17:53:05 -0500

    Add snow to 404 page

--- src/components/scenes/NoMatch.jsx
+++ src/components/scenes/NoMatch.jsx
@@ -8,6 +8,8 @@ import React from 'react';
 
 import Modal from '../Modal.jsx';
 
+require('aframe-particle-system-component');
+
 /**
  * Returns React component that render's a 404 Not Found scene.
  * {@inheritdoc}
@@ -17,17 +19,23 @@ const NoMatch = {
   sky: require('../../assets/images/no-match.png'),
   scene: () => (
     <Entity>
-      <Modal
-        visible
-        id="modal__404"
-        title="404, not found!"
-        actionText="Back"
-        to="#"
-        content={['The scene you are looking for does not exist.',
-          'We\'re so sorry! We\'ll notice this in our analytics, and',
-          'resolve the issue :) Thanks for exploring our VR scenes!'].join(' ')}
-        image={require('../../assets/images/404-cuteness.jpg')}
+      <Entity
+        particle-system="preset: snow; particleCound: 5000;"
+        position={{ x: 0, y: 2.25, z: -15 }}
       />
+      <Entity>
+        <Modal
+          visible
+          id="modal__404"
+          title="404, not found!"
+          actionText="Back"
+          to="#"
+          content={['The scene you are looking for does not exist.',
+            'We\'re so sorry! We\'ll notice this in our analytics, and',
+            'resolve the issue :) Thanks for exploring our VR scenes!'].join(' ')}
+          image={require('../../assets/images/404-cuteness.jpg')}
+        />
+      </Entity>
     </Entity>
   ),
 };
