b73e27abbe7824f7f386080723a3674452f1721a
Patrick Coffey <patrickcoffey48@gmail.com>
2017-04-23 14:12:26 -0400

    Coding standards

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -86,7 +86,6 @@ class Modal extends React.Component {
    * Renders this component.
    */
   render() {
-    console.log(this.state.visible);
     const Image = () => {
       if (this.props.image.length <= 0) {
         return (<div />);
