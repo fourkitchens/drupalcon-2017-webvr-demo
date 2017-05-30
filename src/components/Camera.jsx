f371d34e3ae86b6edce20e221fc8900bb875cfbc
Patrick Coffey <patrickcoffey48@gmail.com>
2017-05-30 18:43:17 -0500

    Reduce fuse timeout and animation

--- src/components/Camera.jsx
+++ src/components/Camera.jsx
@@ -23,7 +23,7 @@ const Camera = props => (
       id="cursor"
       cursor
       fuse
-      fuseTimeout="100"
+      fuseTimeout="10"
       position="0 0 -1"
       color="white"
       geometry={{
@@ -43,7 +43,7 @@ const Camera = props => (
         to="#127218"
         direction="alternate"
         repeat="1"
-        dur="50"
+        dur="300"
       />
     </Entity>
   </Entity>
